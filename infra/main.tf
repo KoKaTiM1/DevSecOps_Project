1508a3680577117ceb7c0ca9f3195430c4792f0e
KoKaTiM1 <progamer0001001001@gmail.com>
2026-04-28 15:43:19 +0300

    fix naming for bucket

--- infra/main.tf
+++ infra/main.tf
@@ -6,7 +6,7 @@ terraform {
     }
   }
   backend "s3" {
-    bucket         = "project-final-DevOpsSec-terraform-state"
+    bucket         = "project-final-devopssec-terraform-state"
     key            = "infra/terraform.tfstate"
     region         = "us-east-1"
     encrypt        = true
